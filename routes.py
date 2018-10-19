57cdafa05dffb0d5ba2f8cdc4aef82b9c75d1a89
James Clark <James2Clark@TAGUKWL13075>
2018-10-19 18:47:21 +0100

    Repository cleanup

--- routes.py
+++ routes.py
@@ -7,8 +7,8 @@ from alert import Alert
 @app.route('/')
 def main():
     data = rest.get_floods()
-    
     alert_list = []
+	fmtdate = datetime.datetime.now().strftime('%c')
 
     for i in range(len(data["items"])):
         level = data["items"][i]["severityLevel"]
@@ -22,7 +22,5 @@ def main():
         alert = Alert(level, river_sea, county, region, message, polygon)
         alert_list.append(alert)
 
-
-    fmtdate = datetime.datetime.now().strftime('%c')
     
     return render_template('alert.html', alerts = alert_list, fmtdate = fmtdate)
